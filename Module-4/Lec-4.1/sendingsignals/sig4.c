b353c646dc3928203645b878512c9f9c29e557a5
Arif Butt <arifpucit@gmail.com>
2025-08-20 13:50:12 +0500

    Updating Lec4.1 codes

---
+++ Module-4/Lec-4.1/sendingsignals/sig4.c
@@ -0,0 +1,24 @@
+/*
+*  Programmer: Arif Butt
+*  Course: Operatin Systems
+*  Parent process sends a signal to child process using kill()
+*/
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(){
+   pid_t cpid = fork();
+   if (cpid ==0){
+      for(;;){
+         printf("I am child in an infinite loop\n");
+         sleep(1);
+      }
+   }
+   else{
+      sleep(5);
+      kill(cpid,SIGSEGV);
+      printf("I have killed my child... Bye\n");
+      exit(0);
+   }
+}
